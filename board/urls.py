ffe772379177b0ac7cca37f02439d02b4b24b768
hyperces <hyperces@naver.com>
2020-11-09 15:00:03 +0900

    20201109 templates 추가

--- board/urls.py
+++ board/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('list/', views.list),
+    path('view/', views.view),
+    path('write/', views.write),
+    path('update/', views.update),
+    path('delete/', views.delete),
 ]
--- board/views.py
+++ board/views.py
@@ -5,4 +5,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello, Django Board')
+    return render(request,'index.html')
+    # return HttpResponse('Hello, Django Board')
+
+
+def list(request):
+    return render(request, 'board/list.html')
+
+
+def view(request):
+    return render(request, 'board/view.html')
+
+
+def write(request):
+    return render(request, 'board/write.html')
+
+
+def update(request):
+    return render(request, 'board/update.html')
+
+
+def delete(request):
+    return render(request, 'board/delete.html')
