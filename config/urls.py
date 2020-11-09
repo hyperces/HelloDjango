c8ef4ee1fa06bbc48862c7ec52f84dce2cff9caa
hyperces <hyperces@naver.com>
2020-11-09 15:16:58 +0900

    20201109 static 적용

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 # from board import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
+    path('admin/', admin.site.urls),
 ]
