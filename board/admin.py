10bf662c411a36dab5e0bb69f7b9ebff16ca78a9
hyperces <hyperces@naver.com>
2020-11-10 13:07:59 +0900

    20201110 django admin 적용

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+
+class BoardAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+class EmployeesAdmin(admin.ModelAdmin):
+    search_fields = ['FIRST_NAME', 'LAST_NAME', 'MANAGER_ID']
+    list_display = ['FIRST_NAME', 'LAST_NAME', 'MANAGER_ID']
+    list_filter = ['HIRE_DATE', 'JOB_ID', 'DEPARTMENT_ID','MANAGER_ID']
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Employees, EmployeesAdmin)
--- board/models.py
+++ board/models.py
@@ -12,3 +12,18 @@ class Board(models.Model):
     views = models.IntegerField(default=0)
     thumbup = models.IntegerField(default=0)
     contents = models.TextField()
+
+
+class Employees(models.Model):
+    # id = models.IntegerField() 자동생성이므로 생략가능
+    EMPLOYEE_ID = models.CharField(max_length=100)
+    FIRST_NAME = models.CharField(max_length=100)
+    LAST_NAME = models.CharField(max_length=100)
+    EMAIL = models.CharField(max_length=100)
+    PHONE_NUMBER = models.CharField(max_length=100)
+    HIRE_DATE = models.DateTimeField()
+    JOB_ID = models.CharField(max_length=100)
+    SALARY = models.IntegerField()
+    COMMISSION_PCT = models.FloatField()
+    MANAGER_ID = models.CharField(max_length=20)
+    DEPARTMENT_ID = models.CharField(max_length=20)
